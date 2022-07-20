db9223e0ee93cb37919ea16b83804c645ba067df
pawali <pawali@binaracademy.org>
2022-07-20 19:24:26 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Add Unit Test OK" });
+});
+
+module.exports = { app };
---
+++ index.js
@@ -0,0 +1,4 @@
+const PORT = 3000
+const { app } = require("./app");
+
+app.listen(PORT, () => console.log("Server running on port", PORT))
